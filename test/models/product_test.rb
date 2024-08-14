c22c255ffff439c8b0317b61f619826b7d0679fb
lewis metcalf <lewisbmetcalf@gmail.com>
2024-08-14 09:14:59 +0100

    test: add product model tests

---
+++ test/models/product_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  link = 'https://www.maxicosi.com/maxicosi_120'
+
+  setup do
+    @brand = Brand.create!(name: 'some-brand')
+    @product_a = create_product! type:Stroller, brand:@brand, link:link
+    @product_b = create_product! type:Stroller, brand:@brand
+    CompatibleLink.create!(product_a: @product_a, product_b: @product_b)
+  end
+
+  test 'should be valid' do
+    assert @product_a.valid?
+  end
+
+  test 'should require a name' do
+    @product_a.name = nil
+    assert_not @product_a.valid?
+  end
+
+  test 'should belong to a brand' do
+    assert_equal @brand, @product_a.brand
+  end
+
+  test 'should return the correct product URL' do
+    assert_equal link, @product_a.link
+  end
+
+  test 'should return the compatible products' do
+    assert_equal [@product_b], @product_a.compatible_products
+  end
+end
