316512089930c2e6062aa2fa518607eca0e6fa29
lewis metcalf <lewisbmetcalf@gmail.com>
2024-08-15 08:08:51 +0100

    test: product controller search types

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,4 +11,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil assigns(:product)
     assert_not_nil assigns(:other_products)
   end
+
+  test "search_types should not include the same type as the product" do
+    get fits_product_url(slug: @product.slug)
+
+    assert_response :success
+    assert_not_nil assigns(:search_types)
+    assert_not_includes assigns(:search_types), @product.productable_type
+    assert_includes assigns(:search_types), "Adapter"
+    assert_includes assigns(:search_types), "Seat"
+  end
 end
