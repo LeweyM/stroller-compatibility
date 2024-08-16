1a7a34ffc2d4207af7f4ddf01a17874695903e4c
lewis metcalf <lewisbmetcalf@gmail.com>
2024-08-16 15:48:39 +0100

    refactor: fix test name creator

--- test/support/utilities.rb
+++ test/support/utilities.rb
@@ -1,7 +1,7 @@
 module Utilities
   def create_product!(type: Stroller, **attributes)
     p = type.create!
-    name = attributes[:name] || type.class.name.split("::").last + "_" + Faker::Commerce.product_name
+    name = attributes[:name] || type.name + "_" + Faker::Commerce.product_name
     brand = attributes[:brand] || brands(:maxicosi)
     Product.create!(name: name, brand: brand, productable: p, **attributes)
   end
