d295f0003bac09f2c734381910252b24e22c96e2
lewis metcalf <lewisbmetcalf@gmail.com>
2024-08-17 17:44:09 +0100

    fix: filter query search bars

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < Admin::BaseController
   def index
+    @brands = Brand.all
     @products = Product.includes(:compatible_products_as_a, :compatible_products_as_b, :brand, :image)
 
     if params[:search].present?
