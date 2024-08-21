23f5753c7d1d7bde8b9913fb3695a70bcb42bee3
lewis metcalf <lewisbmetcalf@gmail.com>
2024-08-21 09:53:30 +0100

    test: improve fake products

--- test/support/utilities.rb
+++ test/support/utilities.rb
@@ -1,8 +1,8 @@
 module Utilities
   def create_product!(type: Stroller, **attributes)
     p = type.create!
-    name = attributes[:name] || type.name + "_" + Faker::Commerce.product_name
-    brand = attributes[:brand] || brands(:maxicosi)
+    name = attributes[:name] || type.name + "_" + Faker::Commerce.product_name + "_" + SecureRandom.hex(4)
+    brand = attributes[:brand] || Brand.create!(name: Faker::Company.name + "_" + SecureRandom.hex(4))
     Product.create!(name: name, brand: brand, productable: p, **attributes)
   end
 end
