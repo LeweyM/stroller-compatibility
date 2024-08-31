c883b056ea09af08a6d17d4d37bed957fb9b6938
lewis metcalf <lewisbmetcalf@gmail.com>
2024-08-31 08:54:05 +0100

    refactor: fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'products/index'
   get 'products/search', to: 'products#search'
-  resources :products, param: :slug, path: '', :only => ['index', 'fits', 'comparison'] do
+  resources :products, param: :slug, path: '', only: [:index] do
     member do
       get :fits
       get 'fits/:b_id', to: 'products#compatible', param: :slug, as: :compatible
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   end
   resources :brands, only: [:index]
   namespace :admin do
-    get '', to: redirect('admin/products')
     resources :compatibility, only: [:index] do
       collection do
         delete :unlink
@@ -44,5 +43,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "brands#index"
+  root "products#index"
 end
