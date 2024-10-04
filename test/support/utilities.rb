7f483bfdc6399f1142ab63bbdc425969c4cde583
lewis metcalf <lewisbmetcalf@gmail.com>
2024-10-04 11:53:51 +0100

    test: improve test helpers

--- test/support/utilities.rb
+++ test/support/utilities.rb
@@ -1,8 +1,9 @@
 module Utilities
   def create_product!(type: Stroller, **attributes)
     p = type.create!
-    name = attributes[:name] || type.name + "_" + Faker::Commerce.product_name + "_" + SecureRandom.hex(4)
+    random_name_suffix = Faker::Commerce.product_name + "_" + SecureRandom.hex(4)
+    name = attributes[:name] ? attributes[:name] + "_" + random_name_suffix : type.name + "_" + random_name_suffix
     brand = attributes[:brand] || Brand.create!(name: Faker::Company.name + "_" + SecureRandom.hex(4))
-    Product.create!(name: name, brand: brand, productable: p, **attributes)
+    Product.create!(brand: brand, productable: p, **attributes, name: name)
   end
 end
