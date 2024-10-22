4ffcd363d08e611d914e5e62f9f5731e8f08b1a7
lewis metcalf <lewisbmetcalf@gmail.com>
2024-10-22 11:50:33 +0100

    fix: enforce image url presence

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :product
 
+  validates :url, presence: true
+
   def attribution_required?
     attribution_url.present? || attribution_text.present?
   end
