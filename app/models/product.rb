9e587db494f8a883d8727ae916d9fc63265702c4
lewis metcalf <lewisbmetcalf@gmail.com>
2024-10-22 11:50:13 +0100

    chore: import file name to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -102,12 +102,12 @@ class Product < ApplicationRecord
     #   delegate to private methods based on filename ending  case file
     start_of_filename = filename.split('_').first.downcase.chomp('.csv')
     case start_of_filename
-    when "strollers", "seats", "adapters" then import_products(file)
+    when "product" then import_products(file)
     when "matrix" then import_matrix(file)
     when "compatible" then import_compatibility(file)
     when "tags" then import_tags(file)
     else
-      allowed_import_file_endings = %w[strollers seats adapters matrix compatible tags]
+      allowed_import_file_endings = %w[product matrix compatible tags]
       raise "Unknown filename '#{filename}'. Filename must begin with one of #{allowed_import_file_endings.join(', ')}"
     end
   end
